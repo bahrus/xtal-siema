f408fde2cbdd5da259dfa31f5f2966c0af6f3ea4
bahrus <anderson.bruce.b@gmail.com>
2018-11-23 20:13:01 -0500

    ...

--- xtal-siema.iife.js
+++ xtal-siema.iife.js
@@ -10,6 +10,10 @@
     customElements.define(tagName, custEl);
 }
 const disabled = 'disabled';
+/**
+ * Base class for many xtal- components
+ * @param superClass
+ */
 function XtallatX(superClass) {
     return class extends superClass {
         constructor() {
@@ -19,20 +23,39 @@ function XtallatX(superClass) {
         static get observedAttributes() {
             return [disabled];
         }
+        /**
+         * Any component that emits events should not do so if it is disabled.
+         * Note that this is not enforced, but the disabled property is made available.
+         * Users of this mix-in should ensure not to call "de" if this property is set to true.
+         */
         get disabled() {
             return this._disabled;
         }
         set disabled(val) {
             this.attr(disabled, val, '');
         }
+        /**
+         * Set attribute value.
+         * @param name
+         * @param val
+         * @param trueVal String to set attribute if true.
+         */
         attr(name, val, trueVal) {
-            const setOrRemove = val ? 'set' : 'remove';
-            this[setOrRemove + 'Attribute'](name, trueVal || val);
+            const v = val ? 'set' : 'remove'; //verb
+            this[v + 'Attribute'](name, trueVal || val);
         }
-        to$(number) {
-            const mod = number % 2;
-            return (number - mod) / 2 + '-' + mod;
+        /**
+         * Turn number into string with even and odd values easy to query via css.
+         * @param n
+         */
+        to$(n) {
+            const mod = n % 2;
+            return (n - mod) / 2 + '-' + mod;
         }
+        /**
+         * Increment event count
+         * @param name
+         */
         incAttr(name) {
             const ec = this._evCount;
             if (name in ec) {
@@ -50,8 +73,14 @@ function XtallatX(superClass) {
                     break;
             }
         }
-        de(name, detail) {
-            const eventName = name + '-changed';
+        /**
+         * Dispatch Custom Event
+         * @param name Name of event to dispatch ("-changed" will be appended if asIs is false)
+         * @param detail Information to be passed with the event
+         * @param asIs If true, don't append event name with '-changed'
+         */
+        de(name, detail, asIs) {
+            const eventName = name + (asIs ? '' : '-changed');
             const newEvent = new CustomEvent(eventName, {
                 detail: detail,
                 bubbles: true,
@@ -61,6 +90,10 @@ function XtallatX(superClass) {
             this.incAttr(eventName);
             return newEvent;
         }
+        /**
+         * Needed for asynchronous loading
+         * @param props Array of property names to "upgrade", without losing value set while element was Unknown
+         */
         _upgradeProperties(props) {
             props.forEach(prop => {
                 if (this.hasOwnProperty(prop)) {
@@ -770,14 +803,14 @@ const easing = 'easing';
 const per_page = 'per-page';
 const start_index = 'start-index';
 const undraggable = 'undraggable';
-const single_drag = 'sinle-drag';
+const single_drag = 'single-drag';
 const threshold = 'threshold';
 const loop = 'loop';
 const selected = 'selected';
 const attr_for_selected = 'attr-for-selected';
 const new_selection = 'new-selection';
 /**
- * <xtal-siema></xtal-siema> is a vanilla-ish wrapper around the Siema caousel api
+ * <xtal-siema></xtal-siema> is a vanilla web component wrapper around the Siema caousel api
  * (https://pawelgrzybek.com/siema/ ).
  *
  * @customElement
